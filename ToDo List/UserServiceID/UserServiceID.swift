2989bb0770d23477665a776d22e7802723da974f
Алексей Гвоздков <aleksejgvozdkov@MacBook-Air.local>
2024-11-20 06:24:15 +0300

    Счётчик ID задач
    Создан метод getNextTaskId, который увеличивает значение счётчика ID задач и сохраняет его в UserDefaults.

--- ToDo List/UserServiceID/UserServiceID.swift
+++ ToDo List/UserServiceID/UserServiceID.swift
@@ -2,12 +2,18 @@ import Foundation
 
 class UserServiceID {
     static let shared = UserServiceID()
+    
     private let userIdKey = "userIdKey"
+    private let taskIdKey = "taskIdKey"
     
     private init() {
         if getUserId() == nil {
             let newUserId = generateNumericUserId()
             saveUserId(newUserId)
+        }
+        
+        if UserDefaults.standard.object(forKey: taskIdKey) == nil {
+            UserDefaults.standard.set(0, forKey: taskIdKey)
         }
     }
     
@@ -31,5 +37,15 @@ class UserServiceID {
         let truncatedNumericString = String(numericString.prefix(9))
         
         return Int(truncatedNumericString) ?? 0
+    }
+    
+    func getNextTaskId() -> Int {
+        let currentTaskId = UserDefaults.standard.integer(forKey: taskIdKey)
+        let nextTaskId = currentTaskId + 1
+        
+        UserDefaults.standard.set(nextTaskId, forKey: taskIdKey)
+        UserDefaults.standard.synchronize()
+        
+        return nextTaskId
     }
 }
